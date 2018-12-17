cd7d4e184c42c370a577c2dcf8acf9605d2bf25d
Jeykumar Sankaran <jsanka@codeaurora.org>
2018-12-17 14:35:05 -0800

    drm/msm/dpu: add display port support in DPU
    Add display port support in DPU by creating hooks
    for DP encoder enumeration and encoder mode
    initialization.
    
    This change is based on the SDM845 Display port
    driver changes[1].
    
    changes in v2:
        - rebase on [2] (Sean Paul)
        - remove unwanted error checks and
          switch cases (Jordan Crouse)
    changes in v3:
        - add dp support after fixing
          the current code base for error logging (Sean Paul)
    changes in v4:
        -  avoid duplicate returns (Jordan Crouse)
        -  get rid of duplicate error logs (Jordan Crouse)
    
    [1] https://lwn.net/Articles/768265/
    [2] https://lkml.org/lkml/2018/11/17/87
    
    Signed-off-by: Jeykumar Sankaran <jsanka@codeaurora.org>
    Reviewed-by: Jordan Crouse <jcrouse@codeaurora.org>
    Reviewed-by: Sean Paul <sean@poorly.run>

--- drivers/gpu/drm/msm/disp/dpu1/dpu_kms.c
+++ drivers/gpu/drm/msm/disp/dpu1/dpu_kms.c
@@ -439,6 +439,31 @@ static int _dpu_kms_initialize_dsi(struct drm_device *dev,
 	return rc;
 }
 
+static int _dpu_kms_initialize_displayport(struct drm_device *dev,
+					    struct msm_drm_private *priv,
+					    struct dpu_kms *dpu_kms)
+{
+	struct drm_encoder *encoder = NULL;
+	int rc;
+
+	if (!priv->dp)
+		return 0;
+
+	encoder = dpu_encoder_init(dev, DRM_MODE_ENCODER_TMDS);
+	if (IS_ERR(encoder)) {
+		DPU_ERROR("encoder init failed for dsi display\n");
+		return PTR_ERR(encoder);
+	}
+
+	rc = msm_dp_modeset_init(priv->dp, dev, encoder);
+	if (rc) {
+		DPU_ERROR("modeset_init failed for DP, rc = %d\n", rc);
+		drm_encoder_cleanup(encoder);
+	}
+
+	return rc;
+}
+
 /**
  * _dpu_kms_setup_displays - create encoders, bridges and connectors
  *                           for underlying displays
@@ -451,12 +476,22 @@ static int _dpu_kms_setup_displays(struct drm_device *dev,
 				    struct msm_drm_private *priv,
 				    struct dpu_kms *dpu_kms)
 {
+	int rc;
+
+	rc = _dpu_kms_initialize_dsi(dev, priv, dpu_kms);
+	if (rc)
+		goto fail;
+
+	rc = _dpu_kms_initialize_displayport(dev, priv, dpu_kms);
+	if (rc)
+		goto fail;
+
 	/**
 	 * Extend this function to initialize other
 	 * types of displays
 	 */
-
-	return _dpu_kms_initialize_dsi(dev, priv, dpu_kms);
+fail:
+	return rc;
 }
 
 static void _dpu_kms_drm_obj_destroy(struct dpu_kms *dpu_kms)
@@ -657,13 +692,20 @@ static void _dpu_kms_set_encoder_mode(struct msm_kms *kms,
 	info.capabilities = cmd_mode ? MSM_DISPLAY_CAP_CMD_MODE :
 			MSM_DISPLAY_CAP_VID_MODE;
 
-	/* TODO: No support for DSI swap */
-	for (i = 0; i < ARRAY_SIZE(priv->dsi); i++) {
-		if (priv->dsi[i]) {
-			info.h_tile_instance[info.num_of_h_tiles] = i;
-			info.num_of_h_tiles++;
+	switch (info.intf_type) {
+	case DRM_MODE_ENCODER_DSI:
+		/* TODO: No support for DSI swap */
+		for (i = 0; i < ARRAY_SIZE(priv->dsi); i++) {
+			if (priv->dsi[i]) {
+				info.h_tile_instance[info.num_of_h_tiles] = i;
+				info.num_of_h_tiles++;
+			}
 		}
-	}
+		break;
+	case DRM_MODE_ENCODER_TMDS:
+		info.num_of_h_tiles = 1;
+		break;
+	};
 
 	rc = dpu_encoder_setup(encoder->dev, encoder, &info);
 	if (rc)
