a2d409c7033bd593ea7523d67d832b60c69505c7
Jonathan Marek <jonathan@marek.ca>
2022-12-26 11:36:47 -0500

    drm/msm/adreno: add support for a740
    Add support for Adreno 740 GPU, which needs a different hwcg table and an
    entry in the gpulist.
    
    Signed-off-by: Jonathan Marek <jonathan@marek.ca>

--- drivers/gpu/drm/msm/adreno/adreno_gpu.h
+++ drivers/gpu/drm/msm/adreno/adreno_gpu.h
@@ -56,7 +56,7 @@ struct adreno_reglist {
 };
 
 extern const struct adreno_reglist a612_hwcg[], a615_hwcg[], a630_hwcg[], a640_hwcg[], a650_hwcg[];
-extern const struct adreno_reglist a660_hwcg[], a690_hwcg[], a730_hwcg[];
+extern const struct adreno_reglist a660_hwcg[], a690_hwcg[], a730_hwcg[], a740_hwcg[];
 
 struct adreno_info {
 	struct adreno_rev rev;
