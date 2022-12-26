9d2978e4c09c2f0c37e9739bf0fd4dd3a1450254
Jonathan Marek <jonathan@marek.ca>
2022-12-26 11:30:27 -0500

    drm/msm: enable timeline semaphores
    Note its not enough to fully implement vulkan timeline semaphores, since
    "wait before signal operation is queued" doesn't work.
    
    Signed-off-by: Jonathan Marek <jonathan@marek.ca>

--- drivers/gpu/drm/msm/msm_drv.c
+++ drivers/gpu/drm/msm/msm_drv.c
@@ -1081,7 +1081,8 @@ static const struct drm_driver msm_driver = {
 				DRIVER_RENDER |
 				DRIVER_ATOMIC |
 				DRIVER_MODESET |
-				DRIVER_SYNCOBJ,
+				DRIVER_SYNCOBJ |
+				DRIVER_SYNCOBJ_TIMELINE,
 	.open               = msm_open,
 	.postclose           = msm_postclose,
 	.dumb_create        = msm_gem_dumb_create,
